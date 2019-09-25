4b81b32b0c51d04768de09742cdc763251f27713
Ilya Sidorchik <ilya@sidorchik.ru>
2019-09-26 00:23:34 +0300

    Make Calculator logic

---
+++ app/src/main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,38 @@
+package com.example.calculator;
+
+public class Calculator {
+
+    public int first;
+    public String operation;
+    public int second;
+
+    public Calculator() {
+        first = 0;
+        operation = "";
+        second = 0;
+    }
+
+    public int calculate() {
+        if (operation.equals("C")){
+            return 0;
+        }
+        else if (operation.equals("+")) {
+            return first + second;
+        }
+        else if (operation.equals("−")) {
+            return first - second;
+        }
+        else if (operation.equals("×")) {
+            return first * second;
+        }
+        else if (operation.equals("÷")) {
+            return first / second;
+        }
+        else if (operation.equals("=")) {
+            return first;
+        }
+        else {
+            return second;
+        }
+    }
+}
